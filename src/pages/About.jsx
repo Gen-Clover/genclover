6a9776ae104328b7c43032a08be8062c4130fb81
gauribindofficial <gauribindofficial@gmail.com>
2026-09-08 18:00:41 +0530

    Test Vercel automatic deployment (#9)
    Co-authored-by: rakesh <mrbind772@gmail.com>

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -77,7 +77,7 @@ const About = () => {
               About <span className="text-white font-bold">Gen</span><span className="text-red-500 font-bold">Clover</span>
             </h1>
             <p className="text-xl text-white/70">
-              GenClover is a technology-driven team focused on building intelligent, scalable, and future-ready digital solutions. We believe technology should not just function. It should create clarity, efficiency, and long-term value for businesses.
+              Gen Clover is a technology-driven team focused on building intelligent, scalable, and future-ready digital solutions. We believe technology should not just function. It should create clarity, efficiency, and long-term value for businesses.
             </p>
           </motion.div>
         </div>
